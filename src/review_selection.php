c5e7edc7ca7dbbbd71f389de495d76bc29337866
mehmetsan <trabzonpower@gmail.com>
2019-05-14 21:11:56 +0300

    Merge branch 'master' of https://github.com/aeyc/Company-Interview-and-Employment-Review-Platform-Database-System

---
+++ src/review_selection.php
@@ -0,0 +1,61 @@
+<!DOCTYPE HTML>
+<!--
+	Landed by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Select Review Type</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+		<div id="page-wrapper">
+
+      <!-- Header -->
+				<header id="header">
+					<nav id="nav">
+						<ul>
+							<li><a href="index.php">Home</a></li>
+							<li><a href="elements.php">Elements</a></li>
+							<li><a href="index.php" class="button primary">Logout</a></li>
+						</ul>
+					</nav>
+				</header>
+
+
+        			<!-- Main -->
+        				<div id="main" class="wrapper style1">
+        					<div class="container">
+        						<header class="major">
+        							<h2>Select the Type of the Review</h2>
+        							<p></p>
+        						</header>
+        						<div class="sign">
+
+                      <div class="col-6 col-12-xsmall">
+                        <ul class="actions fit">
+                          <li><a href="#" class="button primary fit">Salary Review</a></li>
+                        </ul>
+                      </div>
+                      <div class="col-6 col-12-xsmall">
+                        <ul class="actions fit">
+                          <li><a href="#" class="button primary fit">Interview Review</a></li>
+                        </ul>
+                      </div>
+                      <div class="col-6 col-12-xsmall">
+                        <ul class="actions fit">
+                          <li><a href="#" class="button primary fit">Benefits Review</a></li>
+                        </ul>
+                      </div>
+                      <div class="col-6 col-12-xsmall">
+                        <ul class="actions fit">
+                          <li><a href="#" class="button primary fit">General Review</a></li>
+                        </ul>
+                      </div>
+
+	</body>
+</html>
