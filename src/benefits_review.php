0cedc193d2f5f77a42ce06b0a2f4f6dc37162c1c
aeyc <abegumtascioglu@gmail.com>
2019-05-18 23:31:12 +0300

    Benefits review completed

--- src/benefits_review.php
+++ src/benefits_review.php
@@ -38,35 +38,45 @@
 
 
         							<!-- Form -->
-        								<section>
-        									<form method="post" action="#">
-        										<div class="row gtr-uniform gtr-50">
-        											<div class="col-12 col-12-xsmall">
-        												<input type="text" name="employment_status" id="employment_status" value="" placeholder="Employment Status" />
-        											</div>
-        											<div class="col-12 col-12-xsmall">
-        												<input type="text" name="job_title" id="job_title" value="" placeholder="Job Title" />
-        											</div>
-        											<div class="col-12 col-12-xsmall">
-        												<input type="text" name="rating" id="rating" value="" placeholder="Rating" />
-        											</div>
-        											<div class="col-12 col-12-xsmall">
-        												<input type="text" name="location" id="location" value="" placeholder="Location" />
-        											</div>
+											<section>
+												<form method="post" action="#">
+													<div class="row gtr-uniform gtr-50">
+														<div class="col-12">
+															<select name="employment_status" id="employment_status">
+																<option value="">Employment_satatus</option>
+																<option value="0">Working</option>
+																<option value="1">Not Working</option>
+															</select>
+														</div>
+														<div class="col-12 col-12-xsmall">
+															<input type="text" name="job_title" id="job_title" value="" placeholder="Job Title" />
+														</div>
+														<div class="col-12 col-12-xsmall">
+															<input type="text" name="rating" id="rating" value="" placeholder="Rating" />
+														</div>
+														<div class="col-12 col-12-xsmall">
+															<input type="text" name="location" id="location" value="" placeholder="Location" />
+														</div>
+														<div class="col-12 col-12-xsmall">
+															<input type="text" name="salary" id="salary" value="" placeholder="Salary" />
+														</div>
+														<div class="col-12">
+															<textarea name="comment" id="comment" placeholder="Enter your comments about the review here..." rows="6"></textarea>
+														</div>
                               <div class="col-12">
 																<textarea name="opportunities" id="opportunities" placeholder="Opportunities" rows="6"></textarea>
 															</div>
 															<div class="col-12">
-																<textarea name="message" id="message" placeholder="Enter your comments here." rows="6"></textarea>
-															</div>
-															<div class="col-4-xsmall">
-																<input type="radio" id="anonymous" name="anonymous" checked>
-																<label for="anonymous">Anonymous</label>
+																<select name="visibility" id="visibility">
+																	<option value="">Anonimity</option>
+																	<option value="0">Anonim</option>
+																	<option value="1">Not anonim</option>
+																</select>
 															</div>
         											<div class="col-12">
         												<ul class="actions">
         													<li><input type="submit" value="submit" name ="submit" class="primary"/></li>
-        													<li><input type="reset" value="Reset" /></li>
+        													<li><input type="reset" value="reset" /></li>
         												</ul>
         											</div>
         										</div>
@@ -78,3 +88,64 @@
 
         	</body>
         </html>
+				<?php
+				$error='';
+				if(isset($_POST['submit']))
+				{
+					function randomSixDigit() {
+						$conn = mysqli_connect('dijkstra.ug.bcc.bilkent.edu.tr', 'ege.marasli', '8nhmQrdt', 'ege_marasli');
+							$min = 0;
+							$max = 999999;
+
+							$temp = rand (  $min ,  $max );
+
+							$query3 = "SELECT * FROM review WHERE reviewID = '$temp' ";
+							$result3 = $conn-> query($query3);
+							while( $result3 -> num_rows != 0){
+								$temp = rand (  $min ,  $max );
+
+								$query4 = "SELECT * FROM review WHERE reviewID = '$temp' ";
+								$result3 = $conn-> query($query4);
+
+							}
+							return $temp;
+
+					}
+
+								$employment_status = $_POST['employment_status'];
+								$job_title = $_POST['job_title'];
+								$rating = $_POST['rating'];
+								$location = $_POST['location'];
+								$comment = $_POST['comment'];
+								$visibility = $_POST['visibility'];
+								$opportunities = $_POST['opportunities'];
+								$reviewID = randomSixDigit();
+
+								$qu = "SELECT * FROM review WHERE reviewID = '$reviewID' ";
+								$res = $conn-> query($qu);
+								if($res -> num_rows == 0){
+
+									$query = "INSERT INTO review(reviewID,Employment_status,job_title,publish_date,rating,location,comment, visibility)
+														VALUES('$reviewID' , '$employment_status' , '$job_title' , '2008-11-11' , '$rating' , '$location','$comment', '$visibility')";
+
+
+									$result = $conn-> query($query);
+									$query2 = "INSERT INTO benefits_review(reviewID,opportunities)
+														VALUES('$reviewID' , '$opportunities')";
+
+
+									$result2 = $conn-> query($query2);
+
+									$message = "Review is uploaded succesfully";
+									echo "<script type='text/javascript'>alert('$message');
+									window.location = 'home_page.php' </script>";
+								}
+
+								else {
+									$message = "Review Upload is failed!";
+									echo "<script type='text/javascript'>alert('$message');</script>";
+								}
+
+
+				}
+				?>
