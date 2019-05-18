bc11bcb6c351c5a667b2a05bd3c5f56996751e2a
Erdem <ege.marasli@ug.bilkent.edu.tr>
2019-05-18 20:03:42 +0300

    Düzen yeni

--- src/conn.php
+++ src/conn.php
@@ -6,4 +6,46 @@ if(! $conn)
 {
     die('Connection Error!!! ' . mysqli_error());
 }
+
+function randomID_user()
+{
+  $conn = mysqli_connect('dijkstra.ug.bcc.bilkent.edu.tr', 'ege.marasli', '8nhmQrdt', 'ege_marasli');
+
+  if(! $conn)
+  {
+      die('Connection Error!!! ' . mysqli_error());
+  }
+  $min = 0;
+  $max = 999999;
+
+  $temp = rand (  $min ,  $max );
+
+  $query = "SELECT * FROM user WHERE userID = '$temp' ";
+  $result = $conn-> query($query);
+  while( $result -> num_rows != 0){
+    $temp = rand (  $min ,  $max );
+
+    $query = "SELECT * FROM user WHERE userID = '$temp' ";
+    $result = $conn-> query($query);
+
+  }
+  return $temp;
+}
+function isMailExist($email)
+{
+  $conn = mysqli_connect('dijkstra.ug.bcc.bilkent.edu.tr', 'ege.marasli', '8nhmQrdt', 'ege_marasli');
+
+  if(! $conn)
+  {
+      die('Connection Error!!! ' . mysqli_error());
+  }
+
+
+  $query = "SELECT * FROM user WHERE mail = '$email' ";
+  $result = $conn-> query($query);
+  if($result -> num_rows != 0){
+    return True;
+  }
+  return False;
+}
 ?>
