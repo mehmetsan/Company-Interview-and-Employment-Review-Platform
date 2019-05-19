c38f8e6dbd92d5f2d6c6a390f29aa44354d03116
Erdem <ege.marasli@ug.bilkent.edu.tr>
2019-05-19 16:54:33 +0300

    findReviewType()

--- src/conn.php
+++ src/conn.php
@@ -48,4 +48,38 @@ function isMailExist($email)
   }
   return False;
 }
+function findReviewType($reviewID)
+{
+  $conn = mysqli_connect('dijkstra.ug.bcc.bilkent.edu.tr', 'ege.marasli', '8nhmQrdt', 'ege_marasli');
+
+  if(! $conn)
+  {
+      die('Connection Error!!! ' . mysqli_error());
+  }
+
+  $query = "SELECT * FROM salary_review WHERE reveiwID = '$reviewID' ";
+  $result = $conn-> query($query);
+  if( $result -> num_rows != 0){
+    $review_type = "salary_review";
+  }
+
+  $query = "SELECT * FROM interview_review WHERE reveiwID = '$reviewID' ";
+  $result = $conn-> query($query);
+  if( $result -> num_rows != 0){
+    $review_type = "interview_review";
+  }
+
+  $query = "SELECT * FROM benefits_review WHERE reveiwID = '$reviewID' ";
+  $result = $conn-> query($query);
+  if( $result -> num_rows != 0){
+    $review_type = "benefits_review";
+  }
+
+  $query = "SELECT * FROM general_review WHERE reveiwID = '$reviewID' ";
+  $result = $conn-> query($query);
+  if( $result -> num_rows != 0){
+    $review_type = "general_review";
+  }
+  $review_type = "None";
+}
 ?>
