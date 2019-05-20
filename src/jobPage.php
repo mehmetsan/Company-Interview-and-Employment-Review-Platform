b4be06f7832298c070512305cc6e022c346f9032
mehmetselimozcan <selim.ozcan@ug.bilkent.edu.tr>
2019-05-21 00:42:55 +0300

    requested bit

--- src/jobPage.php
+++ src/jobPage.php
@@ -127,6 +127,7 @@ if($result -> num_rows == 1)
       else {
         echo "<a href=\"companyProfile.php\" class=\"button primary\" style=\"text-align:center\">Return To Company Page</a>";
         $_SESSION['jobID'] = $jobID;
+        echo " ";
         echo "<a href=\"applicants.php\" class=\"button primary\" style=\"text-align:center\">See Applicants</a>";
       }
        ?>
