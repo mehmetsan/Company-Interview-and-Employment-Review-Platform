280c534cf67368575e267de3096c24eb97c5e325
aeyc <abegumtascioglu@gmail.com>
2019-05-21 05:51:59 +0300

    panel

--- src/admin_control.php
+++ src/admin_control.php
@@ -19,11 +19,8 @@
 				<header id="header">
 					<nav id="nav">
 						<ul>
-							<li><a href="home_page.php" class ="button primary">Home</a></li>
-              <li><a href="employeeProfile.php" class ="button primary">Profile</a></li>
-              <li><a href="companyList.php" class ="button primary">Companies</a></li>
-              <li><a href="jobList.php" class ="button primary">Jobs</a></li>
-              <li><a href="projectList.php" class ="button primary">Projects</a></li>
+							<li><a href="admin_userList.php" class="button primary fit">Users</a></li>
+							<li><a href="admin_reviewList.php" class="button primary fit">Requested Reviews</a></li>
               <li><a href="index.php" class ="button primary">Logout</a></li>
 						</ul>
 					</nav>
