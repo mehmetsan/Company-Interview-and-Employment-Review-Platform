fb4cf39f8c7773f37c5640029f70735ca8b4e5d0
mehmetselimozcan <selim.ozcan@ug.bilkent.edu.tr>
2019-05-21 08:14:07 +0300

    twrea

--- src/login.php
+++ src/login.php
@@ -12,7 +12,7 @@ https://www.youtube.com/watch?v=J5RHnJCy8AE
 */
 session_start();
 $connection = mysqli_connect('dijkstra.ug.bcc.bilkent.edu.tr', 'ege.marasli', '8nhmQrdt', 'ege_marasli');
-
+$_SESSION['apply']= "false";
 if(! $connection)
 {
     die('Connection Error!!! ' . mysqli_error());
